cdad6925669e3c37f31e982378a9fa12e06ec1e6
ameyaparab1996 <98881388+ameyaparab1996@users.noreply.github.com>
2023-05-23 11:42:21 -0500

    Update streamlit_app.py
    separated the fruit from URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 #Fruityvice API
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Normalize JSON response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
